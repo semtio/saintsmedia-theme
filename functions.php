687778ccaff9ecbce60597bacec9db3126c63e5a
semtio <semik20092@gmail.com>
2025-08-20 21:53:17 +0200

    Cache-Basting temporarily

--- functions.php
+++ functions.php
@@ -10,7 +10,8 @@
 
 if (! defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define('_S_VERSION', '1.0.0');
+	define('_S_VERSION', date('YmdHis')); // бодрим кэш пока ведутся разработки
+	// define('_S_VERSION', '1.0.0');
 }
 
 /**
@@ -147,7 +148,7 @@ function saintsmedia_scripts()
 	wp_enqueue_script('saintsmedia-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
 
 	// 7on подключил собственные CSS стили темы
-	wp_enqueue_style('menu-style', get_template_directory_uri() . '/assets/css/menu-style.css', array('saintsmedia-style'), date('YmdHis'));
+	wp_enqueue_style('menu-style', get_template_directory_uri() . '/assets/css/menu-style.css', array('saintsmedia-style'), _S_VERSION);
 	wp_enqueue_style('footer-style', get_template_directory_uri() . '/assets/css/footer-style.css', array('saintsmedia-style'), _S_VERSION);
 	// Стили для хлебных крошек
 	wp_enqueue_style('breadcrumbs-style', get_template_directory_uri() . '/assets/css/breadcrumbs.css', array('saintsmedia-style'), _S_VERSION);
