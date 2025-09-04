260aa01e27539b1b985c4c6b2e8fc13040d7decb
semtio <semik20092@gmail.com>
2025-09-04 13:27:33 +0200

    Disabled the CSS-JS auto version

--- functions.php
+++ functions.php
@@ -10,8 +10,8 @@
 
 if (! defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define('_S_VERSION', date('YmdHis')); // бодрим кэш пока ведутся разработки
-	// define('_S_VERSION', '1.0.0');
+	// define('_S_VERSION', date('YmdHis')); // бодрим кэш пока ведутся разработки
+	define('_S_VERSION', '1.0.0');
 }
 
 /**
